9360a466346c3363a2112774eaa80c5252e8081a
maxigarrett <maxyg22@outlook.es>
2021-03-12 20:32:25 -0300

    acordeon para celular terminado falta escritorio

--- 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
+++ 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
@@ -1,12 +1,34 @@
 const articleAcordeon=document.querySelectorAll('.article');
+const iconArrowAcordeon=document.querySelectorAll('.article__icon')
 
 articleAcordeon.forEach(element=>{
-	console.log(element);
 	element.addEventListener('click',(e)=>{
 		if(e.target.classList.contains('article__title')){
-			console.log(e.target);
-			e.target.parentElement.classList.toggle('article--show')
-			e.target.classList.toggle('article__title--active');
+			selectTitleOrArrow(e)
 		}
+
 	})
 })
+
+iconArrowAcordeon.forEach(element=>{
+	element.addEventListener('click',(e)=>{
+		selectTitleOrArrow(e)
+	})
+})
+
+//funcion que comprueba si hacemos click en el titulo o en la imagen de flecha para desplegar 
+const selectTitleOrArrow=(e)=>{
+	//este if comprueba si seleccionamos el titulo del articulo tiene que ir porque tambien capturamos la clase de la imagen
+	//si lo sacamos no anda aunque se repita arriba
+	if(e.target.classList.contains('article__title')){
+		e.target.parentElement.classList.toggle('article--show')
+		e.target.classList.toggle('article__title--active');
+		e.target.children[0].classList.toggle('article__icon--active');
+	}
+	if(e.target.classList.contains('article__icon')){
+		console.log(e.target.classList.contains('article__icon'));
+		e.target.parentElement.parentElement.classList.toggle('article--show')
+		e.target.parentElement.classList.toggle('article__title--active');
+		e.target.classList.toggle('article__icon--active');
+	}
+}

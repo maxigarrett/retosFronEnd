7fa86e03f4b3819696dc741245da4bd41f762ae8
maxigarrett <maxyg22@outlook.es>
2021-03-12 04:40:42 -0300

    parte de acordeo js funcionando

--- 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
+++ 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
@@ -1,9 +1,12 @@
 const articleAcordeon=document.querySelectorAll('.article');
 
-
 articleAcordeon.forEach(element=>{
 	console.log(element);
 	element.addEventListener('click',(e)=>{
-		console.log(e.target);
+		if(e.target.classList.contains('article__title')){
+			console.log(e.target);
+			e.target.parentElement.classList.toggle('article--show')
+			e.target.classList.toggle('article__title--active');
+		}
 	})
 })

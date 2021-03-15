d8e231a61ad1fdc45d4168a61bcfbcdf12586b7f
maxigarrett <maxyg22@outlook.es>
2021-03-15 13:31:04 -0300

    acordeon terminado

--- 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
+++ 6-FAC-ACORDEON-MAIN-htm-css-js/js/app.js
@@ -3,6 +3,7 @@ const iconArrowAcordeon=document.querySelectorAll('.article__icon')
 
 articleAcordeon.forEach(element=>{
 	element.addEventListener('click',(e)=>{
+		
 		if(e.target.classList.contains('article__title')){
 			selectTitleOrArrow(e)
 		}
@@ -21,14 +22,41 @@ const selectTitleOrArrow=(e)=>{
 	//este if comprueba si seleccionamos el titulo del articulo tiene que ir porque tambien capturamos la clase de la imagen
 	//si lo sacamos no anda aunque se repita arriba
 	if(e.target.classList.contains('article__title')){
-		e.target.parentElement.classList.toggle('article--show')
-		e.target.classList.toggle('article__title--active');
-		e.target.children[0].classList.toggle('article__icon--active');
+		articleAcordeon.forEach(element=>{
+			console.log(element.childNodes[1].childNodes[1]);
+			element.classList.remove('article--show')
+			element.childNodes[1].classList.remove('article__title--active')
+			element.childNodes[1].childNodes[1].classList.remove('article__icon--active')
+		})
+		if(!e.target.parentElement.classList.contains('article--show')){
+			e.target.parentElement.classList.add('article--show')
+			e.target.classList.add('article__title--active');
+			e.target.children[0].classList.add('article__icon--active');
+		}else{
+			e.target.parentElement.classList.remove('article--show')
+			e.target.classList.remove('article__title--active');
+			e.target.children[0].classList.remove('article__icon--active');
+		}
+		
 	}
 	if(e.target.classList.contains('article__icon')){
 		console.log(e.target.classList.contains('article__icon'));
-		e.target.parentElement.parentElement.classList.toggle('article--show')
-		e.target.parentElement.classList.toggle('article__title--active');
-		e.target.classList.toggle('article__icon--active');
+		articleAcordeon.forEach(element=>{
+			console.log(element.childNodes[1]);
+			element.classList.remove('article--show')
+			element.childNodes[1].classList.remove('article__title--active')
+			element.childNodes[1].childNodes[1].classList.remove('article__icon--active')
+		})
+		if(!e.target.classList.contains('article--show')){
+			e.target.parentElement.parentElement.classList.add('article--show')
+			e.target.parentElement.classList.add('article__title--active');
+			e.target.classList.add('article__icon--active');
+
+		}
+		else{
+			e.target.parentElement.parentElement.classList.remove('article--show')
+			e.target.parentElement.classList.remove('article__title--active');
+			e.target.classList.remove('article__icon--active');
+		}
 	}
 }
